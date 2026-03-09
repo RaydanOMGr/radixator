82ef1a1f80c53e7109593ca4489f2b8000e185bb
GitHub Action <action@github.com>
2026-03-09 19:05:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/raydanomgr/radixator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/raydanomgr/radixator/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.raydanomgr.radixator.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.raydanomgr.radixator.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/raydanomgr/radixator/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/raydanomgr/radixator/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.raydanomgr.radixator.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.raydanomgr.radixator.MyBundle
+import com.github.raydanomgr.radixator.services.MyProjectService
 import javax.swing.JButton
 
 
